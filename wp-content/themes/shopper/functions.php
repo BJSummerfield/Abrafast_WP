fd2bdc1b9edb2cbccc8041728206bf9797da146a
Brian Summerfield <brianjsummerfield@gmail.com>
2019-10-29 15:32:57 -0500

    initial commit

---
+++ wp-content/themes/shopper/functions.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Shopper functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Shopper
+ */
+
+/**
+ * Assign the shopper version to a var
+ */
+$shopper_theme              = wp_get_theme( 'shopper' );
+$shopper_version = $shopper_theme['Version'];
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+
+	$content_width = 980; /* pixels */
+}
+
+$shopper = (object) array(
+	'version' => $shopper_version,
+
+	/**
+	 * Initialize all the things.
+	 */
+	'main'       => require_once 'inc/class-shopper.php',
+	'customizer' => require_once 'inc/customizer/class-shopper-customizer.php',
+);
+
+
+require_once 'inc/shopper-functions.php';
+require_once 'inc/shopper-template-hooks.php';
+require_once 'inc/shopper-template-functions.php';
+require_once 'inc/customizer/include-kirki.php';
+require_once  'inc/customizer/class-shopper-pro-kirki.php';
+
+if ( is_admin() ) {
+
+	$shopper->admin = require 'inc/admin/class-shopper-admin.php';
+}
+
+/**
+ * All for WooCommerce functions
+ */
+if ( shopper_is_woocommerce_activated() ) {
+
+	$shopper->woocommerce = require_once 'inc/woocommerce/class-shopper-woocommerce.php';
+
+	require_once 'inc/woocommerce/shopper-wc-template-hooks.php';
+	require_once 'inc/woocommerce/shopper-wc-template-functions.php';
+}
+
+/* Increase Woocommerce Variation Threshold */
+function wc_ajax_variation_threshold_modify( $threshold, $product ){
+  $threshold = '1111';
+  return  $threshold;
+}
+add_filter( 'woocommerce_ajax_variation_threshold', 'wc_ajax_variation_threshold_modify', 10, 2 );
